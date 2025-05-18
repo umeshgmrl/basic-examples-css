9415cd9e65127e4e44a6e2ef7b398b7e89afbe4a
Bhumesh Domala <umeshgmrl@gmail.com>
2025-05-18 15:12:45 +0530

    Fix PageFromBottom export and Sheet setup

--- src/components/examples/PageFromBottom/PageFromBottom.tsx
+++ src/components/examples/PageFromBottom/PageFromBottom.tsx
@@ -3,34 +3,28 @@ import { Sheet } from "@silk-hq/components";
 import "./PageFromBottom.css";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-   presentTrigger: React.ReactNode;
    sheetContent: React.ReactNode;
 }
 
-const PageFromBottom = ({ presentTrigger, sheetContent, ...restProps }: Props) => {
+export const PageFromBottom = ({ sheetContent, ...restProps }: Props) => {
    return (
-      <Sheet.Root license="commercial" {...restProps}>
-         {presentTrigger}
-         <Sheet.Portal>
-            <Sheet.View
-               className="PageFromBottom-view"
-               contentPlacement="bottom"
-               swipe={false}
-               nativeEdgeSwipePrevention={true}
-            >
-               <Sheet.Backdrop className="PageFromBottom-backdrop" travelAnimation={{ opacity: [0, 0.1] }} />
-               <Sheet.Content className="PageFromBottom-content">
-                  <div className="PageFromBottom-topBar">
-                     <Sheet.Trigger className="PageFromBottom-dismissTrigger" action="dismiss">
-                        Close
-                     </Sheet.Trigger>
-                  </div>
-                  {sheetContent}
-               </Sheet.Content>
-            </Sheet.View>
-         </Sheet.Portal>
-      </Sheet.Root>
+      <Sheet.Portal>
+         <Sheet.View
+            className="PageFromBottom-view"
+            contentPlacement="bottom"
+            swipe={false}
+            nativeEdgeSwipePrevention={true}
+         >
+            <Sheet.Backdrop className="PageFromBottom-backdrop" travelAnimation={{ opacity: [0, 0.1] }} />
+            <Sheet.Content className="PageFromBottom-content">
+               <div className="PageFromBottom-topBar">
+                  <Sheet.Trigger className="PageFromBottom-dismissTrigger" action="dismiss">
+                     Close
+                  </Sheet.Trigger>
+               </div>
+               {sheetContent}
+            </Sheet.Content>
+         </Sheet.View>
+      </Sheet.Portal>
    );
 };
-
-export { PageFromBottom };
