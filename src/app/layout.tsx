29636c6f7b0713bcd0f2d7d4dce343da10caaeb6
Bhumesh Domala <umeshgmrl@gmail.com>
2025-05-18 15:22:04 +0530

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body>
         {children}
-      </body>
     </html>
   );
 }
