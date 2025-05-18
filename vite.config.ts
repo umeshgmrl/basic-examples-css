e5967bbed7d0a5652d7a1d747509aa42bde2ac6c
Bhumesh Domala <umeshgmrl@gmail.com>
2025-05-18 15:56:02 +0530

    Add PWA manifest and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Expense tracker",
+        theme_color: "#f677d2",
+        background_color: "#c7f59b",
+        short_name: "Expense tracker",
+        id: "expense-trakcer-bhumesh",
+        description: "Expense tracker for Bhumesh"
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
